1481e3b36eae58c491890b79a6cfd9303e61a4fa
Mohamedalijas <114052612+Mohamedalijas@users.noreply.github.com>
2025-03-05 10:09:23 +0530

    Add files via upload

--- Config/RabbitMQSettings.cs
+++ Config/RabbitMQSettings.cs
@@ -2,8 +2,11 @@ namespace AljasAuthApi.Config
 {
     public class RabbitMQSettings
     {
-        public string HostName { get; set; } = "localhost";
+        public string HostName { get; set; } = "43.204.40.208";
         public int Port { get; set; } = 5672;
-        public string Exchange { get; set; } = "visitor_events";
+        public string UserName { get; set; } = "myuser";
+        public string Password { get; set; } = "mypassword";
+        public string ExchangeName { get; set; } = "VisitorExchange";
+        // public string QueueName { get; set; } = "visitor.created";
     }
 }
